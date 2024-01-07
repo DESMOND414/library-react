5ef28a304eaa5d47427af8eb18afe9da1c0d7240
Algani Uthej <69247162+DESMOND414@users.noreply.github.com>
2024-01-07 23:08:44 +0530

    project setup

---
+++ 02-frontend/react-library/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layouts/Homepage/ExploreTopBooks';
+import { Carousel } from './layouts/Homepage/Carousel';
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <ExploreTopBooks/>
+    <Carousel/>
+    </div>
+  
+  );
+}
+
+export default App;
